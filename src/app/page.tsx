795d8c826a73ae0f400e37494f55ac4bfaf24340
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-02-26 19:05:22 +0500

    setting up env vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import React from 'react';
+
+const Home = () => {
+  
   return (
     <main>
       <h1>Hello Next with Firebase</h1>
     </main>
   );
 }
+
+export default Home
---
+++ src/constants/env/envVar.tsx
@@ -0,0 +1,12 @@
+const { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } = process.env
+
+const envVar = {
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID
+}
+
+export default envVar
